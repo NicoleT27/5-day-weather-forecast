8db2dfd6161f29c39b841b1f0f392dd3ab8a69cc
Nicole <nicolejoan9@yahoo.com>
2023-08-14 13:41:25 -0400

    Updated ReadMe with screenshots, updated local storage for search history, and updated styling of border

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -16,7 +16,12 @@ var dayOfWeek = document.getElementById("week");
 var currentDate = document.getElementById("currentDate");
 // var day = document.querySelectorAll(".day");
 
+// dayjs is being used to get the current day, date and time to go along with their weather
 var today = dayjs();
+// we are delcaring a searchHistory variable to hold the information for local storage, Whether there is info already
+// saved in local storage or if its empty which will then be a empty array
+var searchHistory =
+  JSON.parse(window.localStorage.getItem("searchHistory")) || [];
 
 // The event listener will only be envoked when the element being clicked
 // is identified as a button to avoid envoking a button by clicking elsewhere
@@ -24,12 +29,18 @@ var today = dayjs();
 cityBtns.addEventListener("click", function (event) {
   if (event.target.classList.contains("btn")) {
     city.innerHTML = event.target.innerText;
+    // Day.js is being envoked
     currentDate.innerHTML = today.format(" dddd MMMM D YYYY" + " " + "hh:mm a");
+    // The next two lines will be envoking functions that will collect information that was fetched from the api
     getCurrentWeather(event.target.innerText);
     getDailyForecast(city.innerHTML);
-    var cityChosen = city.innerHTML;
-    var location = this.getAttribute("id");
-    localStorage.setItem(location, cityChosen);
+    // Then we are going to be adding it to local storage by creating a new variable that is assigned the city the user chose
+    // Then we will add to the end of the searchHistory(.push) the newSearch variable which holds the city the user chose
+    // Laslty we will reset the local storage with the new information by setting the key to search-history and the value
+    // is the stringed version of the array holding the new city chosen
+    var newSearch = city.innerHTML;
+    searchHistory.push(newSearch);
+    localStorage.setItem("search-history", JSON.stringify(searchHistory));
   }
 });
 
@@ -40,16 +51,22 @@ form.addEventListener("submit", function (event) {
   event.preventDefault();
   var input = userInput.value;
   city.innerHTML = input;
+  // Day.js is being envoked
   currentDate.innerHTML = today.format(" dddd MMMM D YYYY" + " " + "hh:mm a");
+  // The next two lines will be envoking functions that will collect information that was fetched from the api
   getCurrentWeather(userInput.value);
   getDailyForecast(city.innerHTML);
-  var cityChosen = city.innerHTML;
-  var location = cityBtns.getAttribute("id");
-  localStorage.setItem(location, cityChosen);
+  // Then we are going to be adding it to local storage by creating a new variable that is assigned the city the user chose
+  // Then we will add to the end of the searchHistory(.push) the newSearch variable which holds the city the user chose
+  // Laslty we will reset the local storage with the new information by setting the key to search-history and the value
+  // is the stringed version of the array holding the new city chosen
+  var newSearch = city.innerHTML;
+  searchHistory.push(newSearch);
+  localStorage.setItem("search-history", JSON.stringify(searchHistory));
 });
 
 // The function is fetching the weather api data via json to display current
-// weather statistics
+// weather statistics for temp, wind, humidity and icon
 function getCurrentWeather(city) {
   var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&cnt=1&appid=${apiKey}&units=imperial`;
   fetch(currentWeatherUrl)
@@ -64,26 +81,24 @@ function getCurrentWeather(city) {
       );
     });
 }
-
+// The function is fetching a diffrent weather api data via json to display 5day forecast
+// weather statistics for temp, wind, humidity and icon
 function getDailyForecast(city) {
   var dailyForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
 
   fetch(dailyForecastUrl)
     .then((response) => response.json())
     .then((data) => {
-      // console.log(data.list[0])
-      // console.log(data.list[8]);
-      // console.log(data.list[16]);
-      // console.log(data.list[24]);
-      // console.log(data.list[32]);
+      // we have a for loop so that it will iterate over the array and grab information for every 8th data set,
+      // which would be 0,8,16,24 and 32 for all 5 days
       var index = 1;
       for (var i = 0; i < data.list.length; i += 8) {
         var days = data.list[i];
-        console.log(days);
 
+        // The weekly forecast is being specifically chosen per specific day with the index
         var weeklyForecast = document.getElementById("weeklyForecast" + index);
 
-        var eachDay = weeklyForecast.parentElement.getAttribute("id");
+        // var eachDay = weeklyForecast.parentElement.getAttribute("id");
 
         var temp = weeklyForecast.querySelector("#temp");
         var wind = weeklyForecast.querySelector("#wind");
@@ -98,6 +113,8 @@ function getDailyForecast(city) {
           "src",
           `https://openweathermap.org/img/wn/${days.weather[0].icon}@2x.png`
         );
+        // The index is envoked again so that after it runs the function and gathers it once it will do it all over again
+        // for each of the days until it stops at 5
         index++;
       }
     });
--- README.md
+++ README.md
@@ -4,18 +4,18 @@ Weather dashboard
 
 ## Description
 
-This webpage allows the user to input their city of choice or select a common city from the side menu and it will display the current temperature, wind speed as well as humidty percentage. It will also provide the 5 day forecast for that selected city and their details as well.
+This webpage allows the user to input their city of choice or select a common city from the side menu and it will display the current temperature, wind speed humidty percentage as well as icon. It will also provide the 5 day forecast for that selected city and their details as well.
 
 ## Usage
 
 At the start of the page you are able to see the current city is blank, insinuating that the user either click a common city or input one.
-![Main page without city ](/Assets/images/)
+![Main page without city ](/Assets/images/Main-page-screenshot.jpg)
 
-The user will then wither pick the common city off of the side menu or type in the city and hitting search
-![Search input/Side Menu](/Assets/images/)
+The user will then either pick the common city off of the side menu or type in the city and hitting search
+![Search input/Side Menu](/Assets/images/select-city-screenshot.jpg)
 
-Once the user has clicked the common city or inputed their prefered city the page will provide the current weather forecast of that day as well as the forecast for the next 5 days
-![Generating Forecast](/Assets/images/)
+Once the user has clicked the common city or inputed their prefered city the page will provide the current weather forecast of that day as well as the forecast for the next 5 days with the temp, wind, humidty and icon for each day
+![Generating Forecast](/Assets/images/forecast-screenshot.jpg)
 
 Link to application:
 <a href="">Github Pages</a>
